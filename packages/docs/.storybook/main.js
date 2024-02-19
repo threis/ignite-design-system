72940d94e36d360d0ec58706b6f27b844260acdc
Thiago <thiagoreis93@gmail.com>
2024-02-19 17:00:26 -0300

    add: stories

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -25,7 +25,7 @@ const config = {
     options: {},
   },
   docs: {
-    autodocs: "tag",
+    autodocs: true,
   },
 };
 export default config;
---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores reprehenderit non rerum sunt dolor laborum nulla quos, voluptatum possimus atque impedit voluptate cumque molestias consequuntur perferendis, ullam quam. Doloremque, iusto!',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
